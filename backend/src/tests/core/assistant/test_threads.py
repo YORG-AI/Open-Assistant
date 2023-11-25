f617c07c3c47c81bbeebf87514df50b41eebb03b
JunkoPF <polyethylenego@gmail.com>
2023-11-25 16:12:26 +0800

    update

--- backend/src/tests/core/assistant/test_threads.py
+++ backend/src/tests/core/assistant/test_threads.py
@@ -3,6 +3,7 @@ from src.core.assistant.threads import Threads
 from src.core.assistant.async_threads import AsyncThreads
 from src.core.assistant.assistant import Assistants
 import asyncio
+
 # def test_threads_create():
 #     # 创建一个 Threads 对象
 #     threads = Threads.create()
@@ -17,7 +18,7 @@ import asyncio
 #     assert threads.config.metadata == {}
 
 # def test_threads_run():
-#     # 创建一个 Threads 对象  
+#     # 创建一个 Threads 对象
 #     threads = Threads.create()
 #     # 创建一个助手并保存到 assistants.yaml 文件
 #     assistant = Assistants.create(name="Test Assistant", model="gpt-4-1106-preview", instructions="run code", tools=[{'type':'code_interpreter'}])
@@ -28,41 +29,58 @@ import asyncio
 #     result = threads.run(assistant.id, "Tell me the answer of 17th fibonacci number plus 20th prime.")
 #     print(result)
 
+
 #     print(threads.config.message_history)
 @pytest.mark.asyncio
 async def test_threads_run_stateful_tool():
-    # 创建一个 Threads 对象  
+    # 创建一个 Threads 对象
     threads = AsyncThreads.create()
     # 创建一个助手并保存到 assistants.yaml 文件
-    assistant = Assistants.create(name="Test Assistant", model="gpt-4-1106-preview", instructions="Use swe tool auto fix code files", tools=[{'type':'swe_tool'}])
+    assistant = Assistants.create(
+        name="Test Assistant",
+        model="gpt-4-1106-preview",
+        instructions="Use swe tool auto fix code files",
+        tools=[{"type": "swe_tool"}],
+    )
     print(assistant.id)
     # 运行 Threads 对象
-    result =await threads.run(assistant.id, "Use SoftWare Engineer Agent swe tool auto fix code files.")
+    result = await threads.run(
+        assistant.id, "Use SoftWare Engineer Agent swe tool auto fix code files."
+    )
     print(result)
 
-    result =await threads.run(assistant.id, "the repo url is https://github.com/YORG-AI/Open-Assistant",goto="stage_1")
+    result = await threads.run(
+        assistant.id,
+        "the repo url is https://github.com/YORG-AI/Open-Assistant",
+        goto="stage_1",
+    )
     print(result)
 
-    result =await threads.run(assistant.id, "add helloworld feature to readme",  goto="stage_2")
+    result = await threads.run(
+        assistant.id, "add helloworld feature to readme", goto="stage_2"
+    )
     print(result)
 
-    result =await threads.run(assistant.id, "focus_files_name_list = [README.md]", goto="stage_3")
+    result = await threads.run(
+        assistant.id, "focus_files_name_list = [README.md]", goto="stage_3"
+    )
     print(result)
 
-    result =await threads.run(assistant.id, "action=3", goto="stage_4")
+    result = await threads.run(assistant.id, "action=3", goto="stage_4")
     print(result)
 
-    result =await threads.run(assistant.id, "", goto="stage_5")
+    result = await threads.run(assistant.id, "", goto="stage_5")
     print(result)
 
-    result =await threads.run(assistant.id, "action=0,action_idx=0", goto="stage_6")
+    result = await threads.run(assistant.id, "action=0,action_idx=0", goto="stage_6")
     print(result)
 
-    result =await threads.run(assistant.id, "", goto="finish")
+    result = await threads.run(assistant.id, "", goto="finish")
     print(result)
+
 
 # def test_threads_run_stateful_tool():
-#     # 创建一个 Threads 对象  
+#     # 创建一个 Threads 对象
 #     threads = Threads.create()
 #     # 创建一个助手并保存到 assistants.yaml 文件
 #     assistant = Assistants.create(name="Test Assistant", model="gpt-4-1106-preview", instructions="Use swe tool auto fix code files", tools=[{'type':'swe_tool'}])
@@ -91,4 +109,3 @@ async def test_threads_run_stateful_tool():
 
 #     result = threads.run(assistant.id, "", goto="finish")
 #     print(result)
-     
